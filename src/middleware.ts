2f4a278a0ab7ba2dfd41e0a12bc36cb83df91b29
Carlos Costa <carllos.nc@gmail.com>
2025-12-08 10:11:25 -0300

    feat: Add authentication middleware for route protection

---
+++ src/middleware.ts
@@ -0,0 +1,15 @@
+import { NextRequest, NextResponse } from "next/server";
+import { headers } from "next/headers";
+import { auth } from "@/lib/auth";
+export async function middleware(request: NextRequest) {
+    const session = await auth.api.getSession({
+        headers: await headers()
+    })
+    if(!session) {
+        return NextResponse.redirect(new URL("/sign-in", request.url));
+    }
+}
+export const config = {
+  runtime: "nodejs",
+  matcher: ["/dashboard"], // Apply middleware to specific routes
+};
