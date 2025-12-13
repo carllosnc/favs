6853221523c502fcc4385705a7166b6b6c343fcb
Carlos Costa <carllos.nc@gmail.com>
2025-12-13 02:19:10 -0300

    Update libs

---
+++ src/lib/error.ts
@@ -0,0 +1,23 @@
+export function extractErrorDetails(errorMessage: string) {
+  console.log(errorMessage)
+
+  const regex =/SQLITE_CONSTRAINT: SQLite error: (\w+) constraint failed: (\w+)\.(\w+)/
+
+  const match = errorMessage.match(regex)
+
+  if (match) {
+    let [, errorName, table, column] = match
+
+    if (errorName == 'UNIQUE') {
+      errorName = 'Already exists'
+    }
+
+    return {
+      table,
+      column,
+      errorName,
+    }
+  }
+
+  return null
+}
--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -4,3 +4,23 @@ import { twMerge } from "tailwind-merge"
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+
+export function getNamespace(email: string) {
+  const namespace = email.split('@')[0]
+  return namespace
+}
+
+export function getSlug(title: string) {
+  const slug = title.toLowerCase().replaceAll(' ', '-')
+  return slug
+}
+
+export function formatDate(date: string) {
+  const formatter = new Intl.DateTimeFormat('en-US', {
+    year: 'numeric',
+    month: 'short',
+    day: 'numeric',
+  })
+
+  return formatter.format(new Date(date))
+}
