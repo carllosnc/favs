61420a37d5d8b2d279152d970ca1623528f6c51f
Carlos Costa <carllos.nc@gmail.com>
2025-12-13 01:46:57 -0300

    feat(db): Rename migration to `0000_early_adam_destine` and add `boxes` and `links` tables.

--- drizzle/0000_early_adam_destine.sql
+++ drizzle/0000_early_adam_destine.sql
@@ -47,3 +47,28 @@ CREATE TABLE `verification` (
 	`created_at` integer DEFAULT (cast(unixepoch('subsecond') * 1000 as integer)) NOT NULL,
 	`updated_at` integer DEFAULT (cast(unixepoch('subsecond') * 1000 as integer)) NOT NULL
 );
+--> statement-breakpoint
+CREATE TABLE `boxes` (
+	`id` text PRIMARY KEY NOT NULL,
+	`title` text NOT NULL,
+	`slug` text NOT NULL,
+	`description` text,
+	`user_id` text,
+	`author_name` text,
+	`author_namespace` text,
+	`is_public` integer DEFAULT 1,
+	`created_at` text DEFAULT 'Wed Dec 10 2025 09:57:22 GMT-0300 (Horário Padrão de Brasília)'
+);
+--> statement-breakpoint
+CREATE UNIQUE INDEX `unique_box` ON `boxes` (`user_id`,`slug`,`author_namespace`);--> statement-breakpoint
+CREATE TABLE `links` (
+	`id` text PRIMARY KEY NOT NULL,
+	`box_id` text NOT NULL,
+	`user_id` text NOT NULL,
+	`created_at` text DEFAULT 'Wed Dec 10 2025 09:57:22 GMT-0300 (Horário Padrão de Brasília)',
+	`title` text,
+	`url` text NOT NULL,
+	`description` text,
+	`image` text,
+	FOREIGN KEY (`box_id`) REFERENCES `boxes`(`id`) ON UPDATE cascade ON DELETE cascade
+);
