54ad32be7286b42281b9bb4184310b333f0fadd7
Carlos Costa <carllos.nc@gmail.com>
2025-12-13 01:50:14 -0300

    Add BoxSheet component to display boxes by namespace in a sheet.

---
+++ src/components/box-sheet.tsx
@@ -0,0 +1,40 @@
+"use client"
+
+import {
+  Sheet,
+  SheetContent,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger,
+} from "@/components/ui/sheet"
+import { Button } from "./ui/button"
+import { useBoxesByNamespace } from "@/data/db-hooks/box-hooks"
+import { Box } from "@/types/db-types"
+import Link from "next/link"
+
+export function BoxSheet({ namespace }: { namespace: string }) {
+  const { data, isLoading } = useBoxesByNamespace(namespace)
+
+  return (
+    <Sheet>
+      <SheetTrigger asChild>
+        <Button variant="outline" size="sm"> Boxes </Button>
+      </SheetTrigger>
+      <SheetContent className="max-w-[250px]" side="left">
+        <SheetHeader>
+          <SheetTitle> Boxes by {namespace} </SheetTitle>
+        </SheetHeader>
+        <div className="px-5 flex flex-col gap-2.5">
+          { data?.map((box: Box) => (
+            <Link
+              key={box.id}
+              href={`/box/${box.author_namespace!}/${box.slug}`}
+              className="text-sm text-blue-600 hover:underline">
+              {box.title}
+            </Link>
+          ))}
+        </div>
+      </SheetContent>
+    </Sheet>
+  )
+}
