b455283ee82f54690fba8375447a6792126d1f42
Carlos Costa <carllos.nc@gmail.com>
2025-12-13 01:56:05 -0300

    Add `DashBoardBoxLinkList` component for displaying links within a dashboard box.

---
+++ src/components/dashboard-box-link-list.tsx
@@ -0,0 +1,33 @@
+import { useLinks } from "@/data/db-hooks/link-hooks"
+import { Box, Link } from "@/types/db-types"
+import { DashboardBoxLinkCard } from "./dashboard-box-link-card"
+
+type Props = {
+  box: Box
+}
+
+export function DashBoardBoxLinkList({ box }: Props) {
+  const { data, isLoading } = useLinks(box.id)
+
+  if (isLoading) {
+    return (
+      <div className="flex flex-col gap-2.5 w-full text-center" >
+        <span className="text-sm"> Loading... </span>
+      </div>
+    )
+  }
+
+  if (data?.length === 0) {
+    return (
+      <div className="flex flex-col text-center gap-2.5 w-full" >
+        <span className="text-sm"> Empty box </span>
+      </div>
+    )
+  }
+
+  return (
+    <div className="flex flex-col gap-2.5">
+      { data?.map((link: Link) => (<DashboardBoxLinkCard key={link.id} link={link} />)) }
+    </div>
+  )
+}
