2385f06df7a0b26c84124cca4d78b335c26da110
Carlos Costa <carllos.nc@gmail.com>
2025-12-13 01:50:01 -0300

    feat: Create BoxContent component to display box details and links

---
+++ src/components/box-content.tsx
@@ -0,0 +1,44 @@
+"use client"
+
+import { useBoxBySlug } from "@/data/db-hooks/box-hooks"
+import { Box } from "@/types/db-types"
+import { BoxLinks } from "./box-links"
+import { BoxSheet } from "./box-sheet"
+import { Switch } from "./ui/switch"
+import { useState } from "react"
+
+type Props = {
+  namespace: string
+  slug: string
+}
+
+export function BoxContent({ namespace, slug }: Props) {
+  const { data, isLoading } = useBoxBySlug(namespace, slug)
+  const [ isTiny, setIsTiny ] = useState<boolean>(false)
+  const box: Box = data?.[0]!
+
+  if (isLoading) {
+    return ( <div /> )
+  }
+
+  return (
+    <section className="flex w-full flex-col items-center flex-1 gap-5">
+      <div className="text-sm text-center">
+        <h1> { box.title } by <b>{ box.author_namespace }</b> </h1>
+        { box.description && <p className="text-sm text-gray-500"> { box.description } </p> }
+      </div>
+
+      <div className="bg-white w-full max-w-[600px] p-2.5 rounded-md shadow-sm flex justify-between items-center">
+        <BoxSheet namespace={namespace} />
+        <div className="flex items-center gap-2.5">
+          <Switch checked={isTiny} id="card" onCheckedChange={() => setIsTiny(!isTiny)} />
+          <label htmlFor="card" className="text-sm text-neutral-500"> Tiny card</label>
+        </div>
+      </div>
+
+      <hr  className="w-full border-neutral-300 max-w-[600px]" />
+
+      <BoxLinks boxId={box.id} isTiny={isTiny} />
+    </section>
+  )
+}
