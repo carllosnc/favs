0a645d7f40c402ad8645d16744333975ad64257d
Carlos Costa <carllos.nc@gmail.com>
2025-12-13 08:53:24 -0300

    Feat: Add logo to 404 page and refine back to home button.

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,16 +1,26 @@
 import { Button } from "@/components/ui/button";
 import Link from "next/link";
+import Image from "next/image"
 
 export default function NotFoundPage() {
   return (
     <div className="flex flex-col  items-center justify-center w-full h-screen">
+
+      <Image
+        className="w-fuill max-w-20 h-auto"
+        src="/images/logo.svg"
+        alt="logo"
+        width={200}
+        height={200}
+      />
+
       <span className="text-neutral-400 font-extralight text-[150px]"> 404 </span>
       <div className="flex flex-col gap-5">
         <span className="text-neutral-500 text-center text-xl">
           Page not found
         </span>
         <Link href="/" className="text-center text-blue-600 text-sm">
-          <Button variant="outline"> ← Go back home </Button>
+          <Button variant="outline" size="sm"> ← back to home </Button>
         </Link>
       </div>
     </div>
