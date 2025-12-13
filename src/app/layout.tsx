7c8fd53a99a3423bf7f57105c769ad8dda88b7d4
Carlos Costa <carllos.nc@gmail.com>
2025-12-13 01:47:43 -0300

    feat: Configure base layout with global styling and integrate React Query & Sonner

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono, Inter } from "next/font/google";
 import "./globals.css";
+import { ReactQueryProvider } from "@/components/react-query-provider";
+import { Toaster } from "@/components/ui/sonner";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -30,9 +32,15 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${interSans.variable} ${geistMono.variable} antialiased`}
+        className={`${interSans.className} ${geistMono.className} antialiased bg-neutral-100`}
       >
-        {children}
+        <div className="w-full m-auto max-w-[800px] border-x border-neutral-300 min-h-screen flex flex-col justify-between items-center lg:border-transparent px-5">
+          <ReactQueryProvider>
+            {children}
+          </ReactQueryProvider>
+        </div>
+
+        <Toaster />
       </body>
     </html>
   );
