8e606c9d4f92d20b284d6b8b5c50cbc4d5b82e89
Carlos Costa <carllos.nc@gmail.com>
2025-12-13 01:47:50 -0300

    feat: Display a spinner for the application loading state.

--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -1,7 +1,9 @@
+import { Spinner } from "@/components/ui/spinner";
+
 export default function Loading(){
   return (
     <div className="w-full h-screen flex justify-center items-center">
-      <h1> Hello world! </h1>
+      <Spinner className="size-8" />
     </div>
   )
 }
