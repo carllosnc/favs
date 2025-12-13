f1d4493a78a538d08463edcb216d9baa92225f97
Carlos Costa <carllos.nc@gmail.com>
2025-12-13 01:48:07 -0300

    feat: Redesign landing page with enhanced content, components, and session integration

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,39 @@
-export default function Home() {
+import Image from "next/image"
+import { auth } from "@/lib/auth";
+import { headers } from "next/headers";
+import { DashboardHeader } from "@/components/dashboard-header";
+import { DashboardFooter } from "@/components/dashboard-footer";
+
+export default async function Home() {
+
+  const session = await auth.api.getSession({
+    headers: await headers(),
+  });
+
   return (
-    <div className="flex justify-center items-center flex-col w-full h-screen">
-      <h1> Landing page </h1>
-    </div>
+    <main className="min-h-screen flex flex-col justify-between">
+      <DashboardHeader session={session} />
+
+      <div className="w-full px-6 flex flex-col gap-[40px] items-center justify-center">
+        <Image
+          className="w-fuill max-w-[120px] h-auto"
+          src="/images/logo.svg"
+          alt="logo"
+          width={200}
+          height={200}
+        />
+
+        <h1 className="md:text-[30px] md:leading-[35px] text-center leading-[50px] font-medium text-[50px] w-full max-w-[700px]">
+          The better way to manage your favorite links
+        </h1>
+
+        <p className="text-center text-neutral-500 w-full text-[20px] md:text-[18px] md:text-md max-w-[600px]">
+          Favs is a sleek, intuitive platform designed to help you
+          effortlessly organize, manage, and share your favorite links.
+        </p>
+      </div>
+
+      <DashboardFooter />
+    </main>
   )
 }
