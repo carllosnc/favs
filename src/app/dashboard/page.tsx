5db1004e0cd346aff4d762c014772180cc641292
Carlos Costa <carllos.nc@gmail.com>
2025-12-13 01:48:20 -0300

    Implement structured dashboard page with dedicated UI components and session integration.

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,11 +1,35 @@
-import { Button } from "@/components/ui/button";
-import{ signOut } from "@/lib/auth-utils"
+import { DashboardFooter } from "@/components/dashboard-footer"
+import { DashboardHeader } from "@/components/dashboard-header"
+import { DashboardNewBox } from "@/components/dashboard-new-box"
+import { DashboardListBoxes } from "@/components/dashboard-list-boxes"
+import { Button } from "@/components/ui/button"
+import { auth } from "@/lib/auth"
+import { headers } from "next/headers"
+import { Session } from "@/types/session"
+import Link from "next/link"
+import { getNamespace } from "@/lib/utils"
 
-export default function Dashboard(){
+export default async function Dashboard(){
+  const session: Session = await auth.api.getSession({
+    headers: await headers(),
+  })
+
   return (
-    <div className="flex flex-col justify-center items-center w-full h-screen">
-      <h1> FAVS </h1>
-      <Button onClick={ signOut }> Sign out </Button>
+    <div className="flex flex-col justify-between items-center w-full min-h-screen">
+      <DashboardHeader session={session} />
+
+      <div className="flex-1 flex w-full pt-5 flex-col gap-5">
+        <div className="flex items-center justify-between gap-2.5 shadow-sm rounded-md bg-white p-2.5">
+          <Link className="inline-flex" href={`/profile/${getNamespace(session!.user.email)}`}>
+            <Button variant="outline" className="cursor-pointer">
+              Public profile
+            </Button>
+          </Link>
+          <DashboardNewBox session={session} />
+        </div>
+        <DashboardListBoxes session={session} />
+      </div>
+      <DashboardFooter />
     </div>
   )
 }
