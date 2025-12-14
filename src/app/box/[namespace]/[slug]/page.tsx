e776b1ee0703276df57de4683cd9a6c2440f18aa
Carlos Costa <carllos.nc@gmail.com>
2025-12-13 21:46:03 -0300

    Remove session check and dashboard button from box page.

--- src/app/box/[namespace]/[slug]/page.tsx
+++ src/app/box/[namespace]/[slug]/page.tsx
@@ -2,19 +2,9 @@ import { BoxContent } from "@/components/box-content"
 import Link from "next/link"
 import Image from "next/image"
 import { DashboardFooter } from "@/components/dashboard-footer"
-import { Session } from "@/types/session"
-import { auth } from "@/lib/auth"
-import { headers } from "next/headers"
-import { Button } from "@/components/ui/button"
-import { FiChevronLeft } from "react-icons/fi"
-import { MdOutlineSpaceDashboard } from "react-icons/md";
 
 export default async function BoxPage( { params }: any ) {
   const { namespace, slug } = await params
-
-  const session: Session = await auth.api.getSession({
-    headers: await headers(),
-  })
 
   return (
     <div className="flex flex-col w-full min-h-screen justify-between items-center gap-5">
@@ -31,14 +21,6 @@ export default async function BoxPage( { params }: any ) {
       <BoxContent namespace={namespace} slug={slug} />
 
       <DashboardFooter />
-
-      { session &&
-        <Link href={`/dashboard/`} className="fixed bottom-5 right-5 leading-5">
-          <Button size="icon-lg" className="cursor-pointer">
-            <MdOutlineSpaceDashboard />
-          </Button>
-        </Link>
-      }
     </div>
   )
 }
