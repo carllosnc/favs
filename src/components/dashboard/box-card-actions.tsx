57d490890a44936c15ca8de59c2472502af387ec
Carlos Costa <carllos.nc@gmail.com>
2025-12-15 14:10:14 -0300

    Feat: Integrate Deletebox component and enhance public page link in box card actions.

--- src/components/dashboard/box-card-actions.tsx
+++ src/components/dashboard/box-card-actions.tsx
@@ -7,12 +7,16 @@ import {
 import { Button } from "../ui/button"
 import { Box } from "@/types/db-types"
 import { MdOutlineMoreVert } from "react-icons/md";
+import Link from "next/link";
+import { Deletebox } from "./delete-box";
+import { Session } from "@/types/session";
 
 type Props = {
   box: Box
+  session: Session
 }
 
-export function BoxCardActions({ box }: Props) {
+export function BoxCardActions({ box, session }: Props) {
   return (
     <DropdownMenu>
       <DropdownMenuTrigger asChild>
@@ -26,12 +30,17 @@ export function BoxCardActions({ box }: Props) {
         </DropdownMenuCheckboxItem>
 
         <DropdownMenuCheckboxItem className="px-2.5">
-          Go to public page
+          <Link href={`/box/${box.author_namespace!}/${box.slug}`}>
+            Go to public page
+          </Link>
         </DropdownMenuCheckboxItem>
 
-        <DropdownMenuCheckboxItem className="px-2.5 text-red-500">
-          Delete box
+        <DropdownMenuCheckboxItem className="p-0" onClick={(e) => { e.preventDefault() }}>
+          <Deletebox box={box} session={session}>
+            <span className="w-full h-full px-2.5 py-1.5 text-red-500"> Delete box </span>
+          </Deletebox>
         </DropdownMenuCheckboxItem>
+
       </DropdownMenuContent>
     </DropdownMenu>
   )
