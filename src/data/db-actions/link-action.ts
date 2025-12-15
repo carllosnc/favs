5c30f8e7b2bdf65a607c5cab34a0e697fe25d6ef
Carlos Costa <carllos.nc@gmail.com>
2025-12-15 14:17:25 -0300

    Maintain box total_links count on link create/delete.

--- src/data/db-actions/link-action.ts
+++ src/data/db-actions/link-action.ts
@@ -1,18 +1,35 @@
 import { db } from '@/database'
 import type { NewLink } from '@/types/db-types'
 import { links } from '@/data/db-schemas/link-schema'
-import { eq, and } from 'drizzle-orm'
+import { boxes } from '@/data/db-schemas/box-schema'
+import { eq, and, sql } from 'drizzle-orm'
 
 export async function createLink(link: NewLink) {
-  return await db.insert(links).values(link).returning()
+  const newLink = await db.insert(links).values(link).returning()
+
+  await db
+    .update(boxes)
+    .set({ total_links: sql`${boxes.total_links} + 1` })
+    .where(and(eq(boxes.id, link.box_id), eq(boxes.user_id, link.user_id)))
+    .returning()
+
+  return newLink
 }
 
 export async function getLinks(boxId: string) {
-  return (
-    await db.select().from(links).where(eq(links.box_id, boxId))
-  ).reverse()
+  const allLinks = (await db.select().from(links).where(eq(links.box_id, boxId))).reverse()
+
+  return allLinks
 }
 
 export async function deleteLink(id: string) {
-  return await db.delete(links).where(eq(links.id, id)).returning()
+  const deleted = await db.delete(links).where(eq(links.id, id)).returning()
+
+  await db
+    .update(boxes)
+    .set({ total_links: sql`${boxes.total_links} - 1` })
+    .where(and(eq(boxes.id, deleted[0].box_id), eq(boxes.user_id, deleted[0].user_id)))
+    .returning()
+
+  return deleted
 }
