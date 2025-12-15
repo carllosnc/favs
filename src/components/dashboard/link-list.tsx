401b5fcc23190d4bfaddc256ec24d4784dfed60a
Carlos Costa <carllos.nc@gmail.com>
2025-12-15 15:44:32 -0300

    Add skeleton loading state for LinkList.

--- src/components/dashboard/link-list.tsx
+++ src/components/dashboard/link-list.tsx
@@ -1,6 +1,7 @@
 import { useLinks } from "@/data/db-hooks/link-hooks"
 import { Box, Link } from "@/types/db-types"
 import { LinkCard } from "./link-card"
+import { Skeleton } from "../ui/skeleton"
 
 type Props = {
   box: Box
@@ -11,8 +12,12 @@ export function LinkList({ box }: Props) {
 
   if (isLoading) {
     return (
-      <div className="flex flex-col gap-2.5 w-full text-center" >
-        <span className="text-sm"> Loading... </span>
+      <div className="flex flex-col gap-2.5 w-full" >
+        {
+          Array(4).fill(0).map((_, i) => (
+            <Skeleton key={i} className="h-20 w-full rounded-md bg-white" />)
+          )
+        }
       </div>
     )
   }
