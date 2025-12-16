6f1536749f1f404c1059ab8456150248d66bb110
Carlos Costa <carllos.nc@gmail.com>
2025-12-16 08:54:13 -0300

    Refactor BoxCard component: replaced main Link wrapper with a div, integrated individual action buttons (edit, delete, view public) directly, and restructured internal layout for better control.

--- src/components/dashboard/box-card.tsx
+++ src/components/dashboard/box-card.tsx
@@ -1,10 +1,13 @@
 import { LuBox, LuLock } from "react-icons/lu"
 import { Box } from "@/types/db-types"
 import { formatDate } from "@/lib/utils"
-import Link from "next/link"
 import { useEffect, useState } from "react"
-import { BoxCardActions } from "./box-card-actions"
 import { Session } from "@/types/session"
+import { EditBox } from "./edit-box"
+import { Button } from "../ui/button"
+import { FiEdit, FiGlobe, FiTrash } from "react-icons/fi"
+import { Deletebox } from "./delete-box"
+import Link from "next/link"
 
 type Props = {
   box: Box
@@ -19,14 +22,15 @@ export function BoxCard({ box, session }: Props) {
   }, [])
 
   return (
-    <Link
-      href={`/dashboard/box/${box.id}`}
-      className="transition-all shadow-sm rounded-md py-[15px] px-5 flex gap-[15px] items-center justify-between bg-white border border-white hover:border-blue-500"
+    <div
+      className="transition-all shadow-sm rounded-md flex items-center justify-between bg-white border-2 border-white"
       key={box.id}>
-      <div className="flex gap-5 items-center w-full">
-        <div className="w-full flex flex-col gap-2.5">
-          <div className="flex items-center gap-5 justify-between">
 
+      <div className="flex items-center w-full">
+        <div className="w-full flex flex-col">
+          <Link
+            href={`/dashboard/box/${box.id}`}
+            className="flex items-center gap-5 justify-between px-5 py-2.5 hover:bg-neutral-50">
             <div className="flex gap-5 items-center">
               {
                 box.is_public
@@ -35,26 +39,36 @@ export function BoxCard({ box, session }: Props) {
               }
               <div className="flex flex-col gap-[5px]">
                 <h3 className="text-neutral-800 font-bold text-[16px]">{box.title}</h3>
-                <p className="text-sm text-neutral-800"> { formatDate(box.created_at!) } </p>
                 {
                   box.description &&
                   <p className="text-sm text-neutral-500"> {box.description} </p>
                 }
+                <p className="text-xs text-neutral-800"> { formatDate(box.created_at!) } </p>
               </div>
             </div>
+          </Link>
 
-            <BoxCardActions box={box} session={session} />
-
-          </div>
           <hr className="w-full" />
-          <div className="flex justify-between">
-            <span className="text-blue-600 text-sm">
-              {url?.host}/box/{box.author_namespace}/{box.slug}
-            </span>
+
+          <div className="flex justify-between items-center px-[15px] py-2.5">
             <span className="text-sm text-neutral-500"> { box.total_links } links </span>
+
+            <div className="flex gap-2.5 items-center">
+              <a href={`/box/${box.author_namespace!}/${box.slug}`} target="_blank" rel="noreferrer">
+                <Button variant="outline" size="icon"> <FiGlobe /> </Button>
+              </a>
+
+              <EditBox session={session} box={box}>
+                <Button variant="outline" size="icon"> <FiEdit /> </Button>
+              </EditBox>
+
+              <Deletebox box={box} session={session}>
+                <Button variant="outline" size="icon"> <FiTrash /> </Button>
+              </Deletebox>
+            </div>
           </div>
         </div>
       </div>
-    </Link>
+    </div>
   )
 }
