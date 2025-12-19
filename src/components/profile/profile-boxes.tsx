4f9a3b1ed2c455079975ebc4dd1a40daa2c43a51
Carlos Costa <carllos.nc@gmail.com>
2025-12-18 21:56:08 -0300

    Re-enable the display of profile boxes by uncommenting the rendering logic.

--- src/components/profile/profile-boxes.tsx
+++ src/components/profile/profile-boxes.tsx
@@ -14,35 +14,35 @@ export function ProfileBoxes({ namespace }: Props) {
 
   if (isLoading) return <Loading />
 
-  // if (boxes?.length === 0) {
+  if (boxes?.length === 0) {
     return (
       <div className="flex-1  w-full flex flex-col items-center gap-2.5">
         <div className="w-full text-sm text-center p-5"> No boxes yet </div>
       </div>
     )
-  // }
+  }
 
-  // return (
-  //   <div className="flex-1 w-full flex flex-col items-center gap-2.5">
-  //     { boxes?.length === 0 && <div className="w-full text-sm text-center p-5"> No boxes yet </div> }
+  return (
+    <div className="flex-1 w-full flex flex-col items-center gap-2.5">
+      { boxes?.length === 0 && <div className="w-full text-sm text-center p-5"> No boxes yet </div> }
 
-  //     {
-  //       boxes?.map((box: Box) => (
-  //         <a
-  //           href={`/box/${namespace}/${box.slug}`}
-  //           key={box.id}
-  //           className="bg-white shadow-sm max-w-[500px] border border-white hover:border-blue-500 transition-all flex items-center gap-[15px] rounded-md w-full text-sm px-[15px] py-[15px] justify-between">
-  //           <div className="flex items-center gap-[15px]">
-  //             <LuBox size={22} className="text-neutral-400" />
-  //             <div className="flex flex-col gap-[3px]">
-  //               <h3 className="font-bold"> {box.title} </h3>
-  //               { box.description && <span className="text-sm text-neutral-500"> {box.description} </span> }
-  //             </div>
-  //           </div>
-  //           <span className="text-sm text-neutral-500"> {box.total_links} links </span>
-  //         </a>
-  //       ))
-  //     }
-  //   </div>
-  // )
+      {
+        boxes?.map((box: Box) => (
+          <a
+            href={`/box/${namespace}/${box.slug}`}
+            key={box.id}
+            className="bg-white shadow-sm max-w-[500px] border border-white hover:border-blue-500 transition-all flex items-center gap-[15px] rounded-md w-full text-sm px-[15px] py-[15px] justify-between">
+            <div className="flex items-center gap-[15px]">
+              <LuBox size={22} className="text-neutral-400" />
+              <div className="flex flex-col gap-[3px]">
+                <h3 className="font-bold"> {box.title} </h3>
+                { box.description && <span className="text-sm text-neutral-500"> {box.description} </span> }
+              </div>
+            </div>
+            <span className="text-sm text-neutral-500"> {box.total_links} links </span>
+          </a>
+        ))
+      }
+    </div>
+  )
 }
