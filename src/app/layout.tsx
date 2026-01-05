ef535919f5984f79d5156bad5597d8ff98086aad
Carlos Costa <carllos.nc@gmail.com>
2026-01-05 07:56:49 -0300

    feat: add initial root layout with global metadata, fonts, and core providers, including `og.png`

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -24,6 +24,29 @@ const interSans = Inter({
 export const metadata: Metadata = {
   title: "FAVS - Your favourite links",
   description: "The better place to your favorite links",
+  openGraph: {
+    title: "FAVS - Your favourite links",
+    description: "The better place to your favorite links",
+    url: "https://favs.vercel.app",
+    siteName: "FAVS",
+    images: [
+      {
+        url: "https://favs.vercel.app/og.png",
+        width: 1200,
+        height: 630,
+        alt: "FAVS - Your favourite links",
+      },
+    ],
+    locale: "en_US",
+    type: "website",
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "FAVS - Your favourite links",
+    description: "The better place to your favorite links",
+    images: ["https://favs.vercel.app/og.png"],
+    creator: "@carlosnosta",
+  },
 };
 
 export default async function RootLayout({
